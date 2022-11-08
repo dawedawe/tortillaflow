3c097a4eeaf4f5080d2058e20644819d69d34736
David Schaefer <david.schaefer@rhein-spree.com>
2022-11-08 23:38:10 +0100

    refactor model

--- src/App.fs
+++ src/App.fs
@@ -28,43 +28,58 @@ module Model =
         | Handsized
 
     type Condition =
-        | Crunchy of SizeAndShape * FillingOrSurrounding
+        | Crunchy
         | Soft
 
     type Comida =
-        | Tortilla of Condition
         | Nachos
         | Taquito
         | Taco
         | EmptyTacoShellForParty
-
         | TortillaSoup
         | Fajita
         | Enchilada
         | Quesadilla
         | ChimiChanga
-
         | Burito
         | Chimichanga
 
-    type Model = { State: Comida }
+    type Model =
+        { Condition: Condition option
+          Folding: Folding option
+          Fried: Fried option
+          FillingOrSurrounding: FillingOrSurrounding option
+          SizeAndShape: SizeAndShape option
+          Comida: Comida option }
 
     type Msg =
-        | AddCheese
-        | AddSalad
+        | ChooseCondition of Condition
+        | ChooseSizeAndShap of SizeAndShape
 
 module State =
 
     open Model
 
     let init () =
-        let model = { State = Comida.Tortilla Condition.Soft }
+        let model =
+            { Condition = None
+              Folding = None
+              Fried = None
+              FillingOrSurrounding = None
+              SizeAndShape = None
+              Comida = None }
+
         (model, Cmd.none)
 
     let update (msg: Msg) (model: Model) =
         match msg with
-        | AddCheese -> { model with State = Fajita }, Cmd.none
-        | AddSalad -> { model with State = Burito }, Cmd.none
+        | ChooseCondition c ->
+            let model' = { model with Condition = Some c }
+            (model', Cmd.none)
+        | ChooseSizeAndShap s ->
+            let model' = { model with SizeAndShape = Some s }
+            (model', Cmd.none)
+        | _ -> System.NotImplementedException() |> raise
 
 module View =
 
@@ -73,14 +88,26 @@ module View =
     open Model
 
     let view model dispatch =
-        [ Bulma.textarea [ prop.value (string model.State) ]
+
+        let description = string model.Condition + "\n" + string model.SizeAndShape
+
+        [ Bulma.textarea [ prop.value description ]
 
           Bulma.button.button
-              [ prop.text "change food with cheese"
-                prop.onClick (fun e -> dispatch Msg.AddCheese) ]
+              [ prop.text "go with a soft tortilla"
+                prop.onClick (fun _ -> Msg.ChooseCondition Condition.Soft |> dispatch) ]
           Bulma.button.button
-              [ prop.text "change food with salad"
-                prop.onClick (fun e -> dispatch Msg.AddSalad) ] ]
+              [ prop.text "go with a crunchy tortilla"
+                prop.onClick (fun _ -> Msg.ChooseCondition Condition.Crunchy |> dispatch) ]
+          Bulma.button.button
+              [ prop.text "small triangles, ovals or rectangles"
+                prop.onClick (fun _ -> Msg.ChooseSizeAndShap SizeAndShape.SmallTrianglesOvalsOrRectangles |> dispatch) ]
+          Bulma.button.button
+              [ prop.text "handsized"
+                prop.onClick (fun _ -> Msg.ChooseSizeAndShap SizeAndShape.Handsized |> dispatch) ]
+          Bulma.button.button
+              [ prop.text "rolled up"
+                prop.onClick (fun _ -> Msg.ChooseSizeAndShap SizeAndShape.RolledUp |> dispatch) ] ]
         |> Fable.React.Helpers.ofList
 
 open State
